05d783d4b951a57e432fce3809ecb527a1aef90c
SuarthaGautama <suartha.gautama@gmail.com>
2018-08-19 12:58:05 +0900

    update information in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ requirements = [
     'tqdm',
     'beautifulsoup4'
 ]
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
    name='mangascandl',
    version='1.0',
    description='Tool for downloading mangascan from websites',
    author='Suartha Gautama',
    author_email='suartha.gautama@gmail.com',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/SuarthaGautama/mangascandl",
    packages=find_packages(),
    entry_points = {
        'console_scripts':[
