4e2e6043db7b8368418d715e39f66fdfbca33771
SuarthaGautama <suartha.gautama@gmail.com>
2018-08-19 13:00:34 +0900

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
    name='mangascandl',
-   version='1.0',
+   version='1.0.1',
    description='Tool for downloading mangascan from websites',
    author='Suartha Gautama',
    author_email='suartha.gautama@gmail.com',
