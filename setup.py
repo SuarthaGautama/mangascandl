45dcce2232578bfc7c9c7228918445595fbc4116
SuarthaGautama <suartha.gautama@gmail.com>
2019-03-03 16:09:06 +0900

    fix installation in windows

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 setup(
    name='mangascandl',
-   version='1.0.4',
+   version='1.0.4.1',
    description='Tool for downloading mangascan from websites',
    author='Suartha Gautama',
    author_email='suartha.gautama@gmail.com',
